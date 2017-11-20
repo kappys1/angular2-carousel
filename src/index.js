6f026f95c1847a4bcd47a4b0860a2e642773da20
alex <alexsbd1@gmail.com>
2017-11-20 12:17:11 +0100

    Iniciar gh-pages

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var carousel_module_1 = require("./carousel.module");
-exports.CarouselModule = carousel_module_1.CarouselModule;
-var carousel_component_1 = require("./carousel.component");
-exports.CarouselComponent = carousel_component_1.CarouselComponent;
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { CarouselModule } from './carousel.module';
-export { CarouselComponent } from './carousel.component';
-
