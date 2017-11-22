ccf151ccb0ef64375c6415482a7a7f1678c5afa0
alex <alexsbd1@gmail.com>
2017-11-22 14:51:43 +0100

    solve move carousel for any device

--- src/carousel.component.ts
+++ src/carousel.component.ts
@@ -229,9 +229,8 @@ export class CarouselComponent implements OnInit,OnChanges,AfterViewInit{
   }
   private rotate(e : any){
     if(!this.carousel.lockSlides) {
-        console.log(e);
         let velocity = this.carousel.isHorizontal ? e.velocityX : -e.velocityY;
-        this.setNewDeg(this.carousel.currdeg + velocity);
+        this.setNewDeg(this.carousel.currdeg + velocity*window.devicePixelRatio);
         this.moveCarrousel(this.carousel.currdeg);
         if (e.isFinal) {
             if (this.endInSlide) {
