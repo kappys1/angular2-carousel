66d7990aecddcedf14f4577fe3e024809c492816
alex <alexsbd1@gmail.com>
2018-05-18 01:48:50 +0200

    change example for ng-for

--- src/app/components/simple-carousel/simple-carousel.component.ts
+++ src/app/components/simple-carousel/simple-carousel.component.ts
@@ -11,6 +11,14 @@ export class SimpleCarouselComponent implements OnInit {
   constructor() {
 
   }
+
+  public images = [
+    "https://dqqzjdqmiszdy.cloudfront.net/sites/default/files/html5_assets/frames_minions_char_3_mob.png",
+    "http://i2.wp.com/farm1.staticflickr.com/502/19162022903_f8cd8501af.jpg?resize=500%2C271&ssl=1",
+    "https://i.pinimg.com/736x/78/1d/29/781d2914510339a762267ed4913cb62b.jpg",
+    "https://www.losminionsaldia.com/images/mas-minions/minion.png"  
+  ]
+
   @ViewChild('topCarousel') topCarousel: CarouselComponent;
 
   toggle(){
