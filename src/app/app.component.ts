3e167fe068ebc2f0a31021042beb7faa3429653e
alex <alexsbd1@gmail.com>
2018-06-03 11:21:12 +0200

    dist improving

--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component, ViewChild} from '@angular/core';
-import {CarouselComponent} from "angular2-carousel";
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-}
